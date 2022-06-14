0314ff31f985ed692a0898aad6190a0e0fb58893
deej197 <jsgdeepikajaganathan@gmail.com>
2022-06-14 20:04:38 +0530

    commit

---
+++ my-workspace/c#/test.cs
@@ -0,0 +1,14 @@
+using System;
+namespace firstpgm
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message="Hello World";
+            Console.WriteLine(message)
+        }
+    }
+    
+}
